51c5a0e7b63d6ffad0ce82189174626cee05b072
soulwax <soulwax-95@protonmail.com>
2025-03-22 16:12:31 +0000

    Add GPL license headers to multiple source files

--- scripts/file_paths_relative-gen.py
+++ scripts/file_paths_relative-gen.py
@@ -1,12 +1,26 @@
-# File: scripts/file_paths_relative-gen.py
+# Copyright (C) 2025 soulwax@github
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 
 # File: scripts/file_paths_relative-gen.py
 
-# File: scripts/file_paths_relative-gen.py
-
-import os
 import fnmatch
+import os
+
 import chardet
+
 
 def should_process(file, whitelist_extensions, ignore_patterns):
     return (any(file.endswith(ext) for ext in whitelist_extensions) and
